b1e33b8923004e241c3908982eb32da5dd1d9f36
meekrabR6R <nmiano84@gmail.com>
2013-08-16 02:54:50 -0400

    Cleaning

--- application/controllers/facebook.php
+++ application/controllers/facebook.php
@@ -2,63 +2,18 @@
 
 class Facebook_Controller extends Base_Controller{
     
-	public $restful = true;
+    public $restful = true;
 	
     public function get_login() {
-
-    	$facebook = Facebook_Controller::getFB();
-		$uid = $facebook->getUser();
-		
-		if($uid){
-			
-			try{
-			     $user = $facebook->api('/me');
-			     $friend_list = $facebook->api(array('method' => 'fql.query',
-						'query' => "SELECT uid,name FROM user WHERE is_app_user = '1'
-						AND uid IN (SELECT uid2 FROM friend
-						WHERE uid1 = '" . $uid . "');"));
-			     
-			     $img_loc = "http://graph.facebook.com/".$uid."/picture";
-			     $_SESSION['user'] = $user;
-			     
-			     Facebook_Controller::new_player($user['id'], $user['first_name'], $user['last_name']);
-
-			     //move to model
-			     $games = array();
-			     foreach(Games::all() as $game){
-			     	$player_count = Plyrgames::where('game_id', '=', $game->game_id)->count();
-			     	array_push($games, array('game_id' => $game->game_id, 'game_title' => $game->title, 'player_count' => $player_count, 'player_max' => $game->plyrs, 'active' => $game->active));
-			     }
-
-			     return View::make('home.index')
-				   ->with('games', $games)
-				   ->with('players', Plyrgames::all())
-				   ->with('player_profiles', Players::all())
-				   ->with('user', $user)
-				   ->with('img_loc', $img_loc)
-				   ->with('list', $friend_list);
-			}
-			catch(FacebookApiException $e){
-				$user = null;
-			
-				$login = $facebook->getLoginUrl();
-		    
-		    	return View::make('home.login')
-		    		->with('login', $login);
-			}
-		}
-		else{
-	
-		    $login = $facebook->getLoginUrl();
-		    //echo '<a href="'.$login.'">LOGIN!</a>';
-		    return View::make('home.login')
-		    		->with('login', $login);
-		    
-		}
-
+    	Facebook_Controller::facebook_login();
     }
 
     public function post_login(){
+    	Facebook_Controller::facebook_login();
+    }
+
+    private static function facebook_login(){
+    	
     	$facebook = Facebook_Controller::getFB();
 		$uid = $facebook->getUser();
 		
@@ -108,9 +63,8 @@ class Facebook_Controller extends Base_Controller{
 		    		->with('login', $login);
 		    
 		}
-
     }
-
+    
     private static function new_player($id, $fn, $ln){
         
         $player_check = Players::find($id);

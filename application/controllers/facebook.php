46ec019670360e13758c560fb16a2c808ce73f2d
meekrabR6R <nmiano84@gmail.com>
2013-12-03 13:49:31 -0500

    Update facebook.php

--- application/controllers/facebook.php
+++ application/controllers/facebook.php
@@ -1,4 +1,3 @@
-
 <?php
 
 class Facebook_Controller extends Base_Controller{
@@ -49,11 +48,11 @@ class Facebook_Controller extends Base_Controller{
           $login = $facebook->getLoginUrl();
           //echo '<a href="'.$login.'">LOGIN!</a>';
           return View::make('home.login')
-              ->with('login', $login);
-		  }
-  }
+                  ->with('login', $login);
+      }
+   }
 
-    public function post_login(){
+   public function post_login(){
         $facebook = Facebook_Controller::getFB();
         $uid = $facebook->getUser();
         if($uid){
@@ -69,20 +68,20 @@ class Facebook_Controller extends Base_Controller{
 			  
                 Facebook_Controller::new_player($user['id'], $user['first_name'], $user['last_name']);
                 //move to model
-               $games = array();
-               foreach(Games::all() as $game){
-                   $player_count = Plyrgames::where('game_id', '=', $game->game_id)->count();
-                   array_push($games, array('game_id' => $game->game_id, 'game_title' => $game->title, 'player_count' => 					
-                   	          $player_count, 'player_max' => $game->plyrs, 'active' => $game->active));
-			         }
+                $games = array();
+                foreach(Games::all() as $game){
+                    $player_count = Plyrgames::where('game_id', '=', $game->game_id)->count();
+                    array_push($games, array('game_id' => $game->game_id, 'game_title' => $game->title, 'player_count' => 					
+                     	          $player_count, 'player_max' => $game->plyrs, 'active' => $game->active));
+                }
 
-               return View::make('home.index')
-               ->with('games', $games)
-               ->with('players', Plyrgames::all())
-               ->with('player_profiles', Players::all())
-               ->with('user', $user)
-               ->with('img_loc', $img_loc)
-               ->with('list', $friend_list);
+                return View::make('home.index')
+                ->with('games', $games)
+                ->with('players', Plyrgames::all())
+                ->with('player_profiles', Players::all())
+                ->with('user', $user)
+                ->with('img_loc', $img_loc)
+                ->with('list', $friend_list);
           }
           catch(FacebookApiException $e){
               $user = null;
@@ -96,8 +95,8 @@ class Facebook_Controller extends Base_Controller{
           $login = $facebook->getLoginUrl();
           //echo '<a href="'.$login.'">LOGIN!</a>';
           return View::make('home.login')
-              ->with('login', $login);
-		  }
+                 ->with('login', $login);
+      }
     }
 
     private static function new_player($id, $fn, $ln){
@@ -112,7 +111,7 @@ class Facebook_Controller extends Base_Controller{
 	        );
 	        
 	        Players::create($new_player);
-	    }
+       }
     }
 }
 
